5fd8e978299a050ae84c7db011c0dce834c2b4a0
XGS - XanTiuM <xantium.xgs@gmail.com>
2026-02-06 18:30:21 +0100

    site name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://developper.dyingstar-game.com/',
+  url: 'https://xantium-dev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
