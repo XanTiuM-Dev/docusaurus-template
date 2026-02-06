f30633ce4b45fe1946df1b0958105b0c7d1c6610
XGS - XanTiuM <69990110+XanTiuM-Dev@users.noreply.github.com>
2026-02-06 18:33:00 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   url: 'https://xantium-dev.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-template/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
